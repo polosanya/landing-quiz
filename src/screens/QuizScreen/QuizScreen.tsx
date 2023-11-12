bea788ba45b0c839f55c148dc40a570704e9b24b
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2023-11-12 20:55:57 +0200

    primary button added

--- src/screens/QuizScreen/QuizScreen.tsx
+++ src/screens/QuizScreen/QuizScreen.tsx
@@ -2,6 +2,8 @@ import { Answer, Question } from "@helpers/types";
 import styles from "./QuizScreen.module.scss";
 import { FC, useRef, useState } from "react";
 import Option from "@components/Option";
+import ButtonPrimary from "@components/ButtonPrimary";
+import Navigation from "@components/Navigation";
 
 type Props = {
   questions: Question[];
@@ -31,6 +33,9 @@ const QuizScreen: FC<Props> = ({ questions }) => {
     
     if (currentQuestion.id === questions.length) {
       console.log("FINISH", statistics.current);
+      // you can send the statistics somewhere if needed;
+
+
     } else {
       setCurrentQuestion((prevValue) => questions[prevValue.id]);
     }
@@ -38,6 +43,8 @@ const QuizScreen: FC<Props> = ({ questions }) => {
 
   return (
     <div className={styles.QuizScreen}>
+      <Navigation />
+
       <h1>
         {currentQuestion.text} {currentQuestion.id}
       </h1>
@@ -56,13 +63,11 @@ const QuizScreen: FC<Props> = ({ questions }) => {
         );
       })}
 
-      <button
+      <ButtonPrimary
+        text="Continue"
         disabled={selectedOptions.length === 0}
-        className={styles.QuizScreen__button}
         onClick={handleContinue}
-      >
-        Continue
-      </button>
+      />
     </div>
   );
 };
